57f6deaf3e72dfdb096f075dd93b079446853122
Jack Bernier <jack_bernier@brown.edu>
2018-06-19 15:40:07 -0400

    fix

---
+++ button1.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var element = document.getElementById("lines")
+    element.style.color = "red";
+    element.style.fontFamily = "Arvo", Helvetica, sans-serif;
+}
+
+function myFunction2() {
+    var element = document.getElementById("lines")
+    element.style.color = "black";
+    element.style.fontFamily = "Times New Roman";
+}
