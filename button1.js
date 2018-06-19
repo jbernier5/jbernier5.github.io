915c19b50e98662772965243949f681c1b7fd6e6
Jack Bernier <jack_bernier@brown.edu>
2018-06-19 17:21:38 -0400

    fix

--- button1.js
+++ button1.js
@@ -9,3 +9,10 @@ function myFunction2() {
     element.style.color = "black";
     element.style.fontFamily = "Times New Roman";
 }
+
+function myFunction3() {
+    var phrasearray = ["a genius!", "really, really, really, ridiculously good-looking.", "not a doctor!", "a pet psychiatrist.", "a professional killer.", "a part of the Illuminati", "a good boy", "not the father!", "not your father Luke", "a wizard?"];
+    var rand = phrasearray[Math.floor(Math.random() * phrasearray.length)];
+    didi = String("Hi I am " + rand)
+    document.getElementById("lines").innerHTML = didi;
+}
